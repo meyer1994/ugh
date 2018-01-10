8948c3f81d3893be47da290d1b0ed0bc2d128674
João Vicente Meyer <1994meyer@gmail.com>
2018-01-10 14:41:20 -0200

    Changed other tests to obey pycodestyle. Added a button test.

--- tests/others/conversion.py
+++ tests/others/conversion.py
@@ -8,15 +8,16 @@ That is a good behaviour, at least I think.
 
 import urwid
 
-texts = [ urwid.Text('nice%2d' % i) for i in range(10) ]
+texts = [urwid.Text('nice%2d' % i) for i in range(10)]
 texts = tuple(texts)
+
 
 def exit_q(key):
     if key == 'q':
         raise urwid.ExitMainLoop()
 
+
 pile = urwid.Pile(texts)
 
 q = pile.contents
 print(isinstance(q, list))
-
